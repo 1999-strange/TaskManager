e824147a843fd40419266e93c68acf7b88b3bf3d
1999-strange <3276866508@qq.com>
2025-01-16 07:22:29 +0800

    New Adaption

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@ import TaskManager from './TaskManager'
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100">
-      <TaskManager />
-    </div>
+    <TaskManager />
   )
 }
 
